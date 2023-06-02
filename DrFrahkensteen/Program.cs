699ca868fdeba0facf155a1ea305aaa4f3922d0b
Peter Culver <pculver722@sbcglobal.net>
2023-06-02 16:26:45 -0400

    Set up the CLASS - Monster within the program.  Fields/Properties/Constructor are all defined.

---
+++ DrFrahkensteen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DrFrahkensteen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
